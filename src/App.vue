0d4d3841e5f39f8c89fef78e84c44c02938d8acc
hunter-s70 <serg.buhtoyarov@gmail.com>
2018-01-25 09:02:05 +0300

    add vuex, bootstrap-vue in project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div id="app" class="container">
     <router-view/>
   </div>
 </template>
 
 <script>
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 export default {
   name: 'App'
 }
--- src/main.js
+++ src/main.js
@@ -3,13 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+import BootstrapVue from 'bootstrap-vue'
 
-Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
-})
+});
