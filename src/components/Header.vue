b105d9641920fc8b5698726744a28beb66f9f363
hunter-s70 <serg.buhtoyarov@gmail.com>
2018-01-30 23:40:26 +0300

    add rules eslint, add app-header component

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="nav">
+    <li class="nav-item">
+      <a class="nav-link active" href="#">Portfolio</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Stocks</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">End Day</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link disabled" href="#">Save & Load</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
