63457b33d3af0e27c907ae64fdd258b46832b349
hunter-s70 <serg.buhtoyarov@gmail.com>
2018-02-03 12:13:20 +0300

    add header markup bs -v 4

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,24 +3,42 @@
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Stock Trader</a>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="#">Portfolio <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">Stocks</a>
         </li>
         <li class="nav-item">
           <a class="nav-link disabled" href="#">Disabled</a>
         </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="text" placeholder="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+      <ul class="navbar-nav mx-2">
+        <li class="nav-item">
+          <a class="nav-link" href="#">End Day</a>
+        </li>
+        <li class="nav-item">
+          <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle"
+                    type="button"
+                    id="dropdownMenuButton"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false">
+              Save & Load
+            </button>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Save Data</a>
+              <a class="dropdown-item" href="#">Load Data</a>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <strong class="navbar-text">Funds: 12 UAH</strong>
     </div>
   </nav>
 </template>
