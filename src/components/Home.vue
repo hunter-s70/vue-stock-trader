c41adb78552638771cb6704dabc29ee9e0c5be62
hunter-s70 <serg.buhtoyarov@gmail.com>
2018-02-03 17:54:41 +0300

    add Home component markup

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,17 @@
 <template>
-  <p>homepage</p>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="card text-left">
+        <div class="card-block">
+          <h2 class="card-title">Trade or View your Portfolio</h2>
+          <p class="card-text header-card-text">You may Save & Load your Data</p>
+          <p class="card-text header-card-text">Click to the 'End Day' to begin a new Day!</p>
+          <hr/>
+          <strong class="card-text">Funds: 12 UAH</strong>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,4 +24,8 @@ export default {
 </script>
 
 <style scoped>
+  .header-card-text {
+    font-size: .9rem;
+    line-height: .5;
+  }
 </style>
