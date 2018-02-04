1cdf0504d61ea47dbd99e6a6b2558b70d029ffe1
hunter-s70 <serg.buhtoyarov@gmail.com>
2018-02-04 10:28:26 +0300

    add Stocks components markup

--- src/components/Stocks.vue
+++ src/components/Stocks.vue
@@ -1,5 +1,17 @@
 <template>
-  <p>stocks</p>
+  <div class="row">
+    <div class="col-md-6 mb-4">
+      <div class="card text-left">
+        <h3 class="card-header bg-green">BMW&nbsp;<small class="text-muted">(price: 200)</small></h3>
+        <div class="card-block d-flex justify-content-between align-items-start">
+          <div class="form-group w-50 m-0">
+            <input type="text" class="form-control" id="quantity" placeholder="Quantity">
+          </div>
+          <a href="#" class="btn btn-success">Buy</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,4 +24,7 @@ export default {
 </script>
 
 <style scoped>
+  .bg-green {
+    background-color: #D8FFD3;
+  }
 </style>
