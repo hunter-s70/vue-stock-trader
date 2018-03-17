6c580294f631ded5482aa3eb9e681de537fe4b8d
hunter-s70 <serg.buhtoyarov@gmail.com>
2018-03-17 09:12:28 +0300

    add portfolio markup

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,5 +1,17 @@
 <template>
-  <p>portfolio</p>
+  <div class="row">
+    <div class="col-md-6 mb-4">
+      <div class="card text-left">
+        <h3 class="card-header bg-blue">BMW&nbsp;<small class="text-muted">(price: 200 | quantity: 10)</small></h3>
+        <div class="card-block d-flex justify-content-between align-items-start">
+          <div class="form-group w-50 m-0">
+            <input type="text" class="form-control" id="quantity" placeholder="Quantity">
+          </div>
+          <a href="#" class="btn btn-danger">Sell</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,4 +24,7 @@ export default {
 </script>
 
 <style scoped>
+  .bg-blue {
+    background-color: #d8e3ff;
+  }
 </style>
